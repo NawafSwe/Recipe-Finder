3930ae0f4a4c7710cdf71f4452ae3fb2ea478b82
NawafSwe <nawaf.jr9@hotmail.com>
2020-11-19 09:08:42 +0300

    Initial Commit

---
+++ Recipe Founder/Recipe_FounderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_FounderApp.swift
+//  Recipe Founder
+//
+//  Created by Nawaf B Al sharqi on 19/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_FounderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
