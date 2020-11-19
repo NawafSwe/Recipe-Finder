0037321ced047b3521ddefc5d3d47f4f977fa875
NawafSwe <nawaf.jr9@hotmail.com>
2020-11-19 19:31:13 +0300

    adding card view

--- Recipe Founder/HomeView.swift
+++ Recipe Founder/HomeView.swift
@@ -10,9 +10,12 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         NavigationView{
-            Text(" This isHome View")
+            VStack{
+                CardView()
+                Spacer()
+            }.padding(.vertical,70)
+            .navigationBarTitle("Home ☘️")
             
-                .navigationBarTitle("Home View😄")
         }
         
     }
@@ -20,6 +23,43 @@ struct HomeView: View {
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
+        
         HomeView()
+    }
+}
+
+
+//MARK:- Title Text View
+struct TitleTextView : View {
+    var body: some View{
+        VStack{
+            Spacer()
+            Text("Search Recipes By Ingredients")
+                .font(.system(size:16))
+                .bold()
+                .fontWeight(.semibold)
+                .foregroundColor(.black)
+                .multilineTextAlignment(.center)
+            
+        }
+        .padding(.bottom,20)
+        .padding(.leading)
+    }
+}
+
+
+struct CardView: View {
+    var body: some View {
+        VStack{
+            Image("galshir-paper-boat")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 320, height: 300, alignment: .center)
+                .cornerRadius(40)
+        }
+        .overlay(TitleTextView(),alignment: .center)
+        .shadow(radius: 10)
+        
+        
     }
 }
