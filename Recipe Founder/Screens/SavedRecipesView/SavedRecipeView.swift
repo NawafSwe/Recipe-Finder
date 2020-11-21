27cef2dacee529e502e532a42e35d4d3d920d7a4
NawafSwe <nawaf.jr9@hotmail.com>
2020-11-21 04:08:18 +0300

    crating empty state for recipes

--- Recipe Founder/Screens/SavedRecipesView/SavedRecipeView.swift
+++ Recipe Founder/Screens/SavedRecipesView/SavedRecipeView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct SavedRecipeView: View {
     var body: some View {
         NavigationView {
-            Text("Saved Recipe View")
+            EmptySavedRecipesStateView()
                 .navigationBarTitle("Saved Recipes 🧾")
         }
     }
@@ -19,5 +19,24 @@ struct SavedRecipeView: View {
 struct SavedRecipeView_Previews: PreviewProvider {
     static var previews: some View {
         SavedRecipeView()
+    }
+}
+
+//MARK:- EmptySavedRecipesStateView
+struct EmptySavedRecipesStateView:View {
+    var body: some View{
+        ZStack{
+            VStack(spacing:10){
+                Image("Card5")
+                    .resizable()
+                    .frame(width: 250 , height: 250)
+                Text("You Do Not Have Saved Recipes Currently Go And Pick Some!")
+                    .foregroundColor(.primary)
+                    .multilineTextAlignment(.center)
+                    .font(.subheadline)
+                    .padding()
+            }
+        }
+        .offset(y:-30)
     }
 }
