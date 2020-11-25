afc3bbb8950d7c3573d2d10126c71df67caff7e8
NawafSwe <nawaf.jr9@hotmail.com>
2020-11-25 04:59:26 +0300

    fix: removing the code in the life cycle app

--- Recipe Founder/Screens/RecipeDetailsView/RecipeDetailsViewModel.swift
+++ Recipe Founder/Screens/RecipeDetailsView/RecipeDetailsViewModel.swift
@@ -18,25 +18,27 @@ final class RecipeDetailsViewModel:ObservableObject{
     func saveRecipe(recipe: RecipeModel?){
         guard let safeRecipe = recipe else {
             return }
+       // print(safeRecipe)
         do{
-            /// making core recipe refers to the moc where to safe
+            /// making core recipe refers to the moc where to save
             let coreRecipe = Recipe(context: self.moc)
-            coreRecipe.id = Int16(safeRecipe.id)
+            coreRecipe.id = Int64(safeRecipe.id)
             coreRecipe.title = safeRecipe.title
             coreRecipe.sourceUrl = safeRecipe.sourceUrl
-            coreRecipe.cookingMinutes = Int16(safeRecipe.cookingMinutes ?? 0)
+            coreRecipe.cookingMinutes = Int64(safeRecipe.cookingMinutes ?? 0)
             coreRecipe.glutenFree = safeRecipe.glutenFree ?? false
             coreRecipe.vegan = safeRecipe.vegan ?? false
             coreRecipe.vegetarian = safeRecipe.vegetarian ?? false
-            coreRecipe.healthScore = Int16(safeRecipe.healthScore ?? 0)
-            coreRecipe.preparationMinutes = Int16(safeRecipe.preparationMinutes ?? 0)
-            coreRecipe.likes = Int16(safeRecipe.likes ?? 0)
-            coreRecipe.image = safeRecipe.image ?? nil
+            coreRecipe.healthScore = Int64(safeRecipe.healthScore ?? 0)
+            coreRecipe.preparationMinutes = Int64(safeRecipe.preparationMinutes ?? 0)
+            coreRecipe.likes = Int64(safeRecipe.likes ?? 0)
+            /// nils are not allowed in the core data so putting placeholder is good choice
+            coreRecipe.image = safeRecipe.image ?? ""
             coreRecipe.veryHealthy = safeRecipe.veryHealthy ?? false
             try moc.save()
             
         }catch let error {
-            print(error)
+            print(error.localizedDescription)
             return
         }
     }
